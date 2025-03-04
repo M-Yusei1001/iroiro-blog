cac5285e74d388adc5a9744754bd6ef6fbceaabc
M-Yusei1001 <bitterchocolate1001@gmail.com>
2025-03-04 14:45:20 +0000

    wip: ヘッダー作成

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-
-    </div>
-  );
+	return (
+		<div>
+			<div className="z-10 fixed navbar bg-base-200 shadow-sm">
+				<div className="flex flex-row justify-between items-center mx-auto w-4/5 text-gray-800">
+					<div className="">
+						<a href="#" className="text-2xl font-semibold">川のほとり</a>
+					</div>
+					<div className="">
+						<a href="#" className="font-semibold">About</a>
+						<a href="#" className="font-semibold ml-4">Posts</a>
+					</div>
+				</div>
+			</div>
+			<div className="text-4xl mx-auto w-4/5 pt-20">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet atque reiciendis ipsa laboriosam laudantium, esse molestiae mollitia at qui optio odio fuga veniam, obcaecati animi aliquam eligendi et. Nisi, dicta.
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae consequatur quibusdam unde sunt voluptatibus debitis natus est cupiditate eaque rerum? Soluta nulla eligendi dicta itaque beatae architecto corporis quaerat asperiores libero quisquam eius eum voluptatem iusto voluptates minus cupiditate, velit ab. Molestiae non et praesentium distinctio sed sit, ratione quod commodi nemo perferendis impedit quo porro, quam dolorum excepturi labore similique eveniet animi veniam, sint delectus voluptatem. Porro, aliquam quasi?
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, nostrum! Sequi, officiis. Aliquid nostrum recusandae perferendis eius, quod, minima at assumenda soluta hic obcaecati quidem! Ducimus at aliquid vitae non voluptas eaque architecto impedit quo nostrum, error beatae eum quia rem facilis minima? Ut aspernatur praesentium provident. Iusto, sapiente, iure vero praesentium excepturi quia, provident eos quidem eligendi enim illum voluptatum facilis odit. Nesciunt accusantium, officia aperiam, dicta, ipsum sit cumque assumenda corrupti mollitia corporis quae cum consequatur natus labore? Corporis, odit ipsa ratione tempore maiores totam dolor eos nihil dolores suscipit iusto aspernatur, repellat commodi labore voluptates sint reiciendis?
+			</div>
+		</div>
+	);
 }
